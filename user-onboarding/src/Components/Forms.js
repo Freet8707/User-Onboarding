87526986247fa4003d1653cea7d846c386a31205
Freet8707 <freet8707@gmail.com>
2020-06-09 22:44:20 -0500

    handle change and submit form functions initiated and form schema validation successfully enables submit button

--- user-onboarding/src/Components/Forms.js
+++ user-onboarding/src/Components/Forms.js
@@ -17,6 +17,8 @@ function Form() {
         terms: ""
     })
 
+    const [buttonDisabled, setButtonDisabled] = useState(true)
+
     const formSchema = yup.object().shape({
         name: yup
             .string()
@@ -33,20 +35,45 @@ function Form() {
             .oneOf([true], 'must agree to terms and conditions')
     })
     
+    const handleChange = e => {
+        e.persist()
+        console.log('name of input that fired event', e.target.name)
+        const newForm = {
+            ...newUser,
+            [e.target.name]: e.target.type === 'checkbox' ? e.target.checked : e.target.value
+        }
+
+        setNewUser(newForm)
+    }
+
+    useEffect(() => {
+        formSchema.isValid(newUser).then(valid => {
+            setButtonDisabled(!valid)
+        })
+    }, [newUser])
+
+    const submitForm = e => {
+        e.preventDefault();
+        console.log('form submitted')
+    }
+
     return (
         <div>
-            <label htmlFor='name'>Name: <br />
-                <input type='text' id='name' name='name' placeholder='Enter Name' />
-            </label><br />
-            <label htmlFor='email'>Email: <br />
-                <input type='text' id='email' name='email' placeholder='Enter Email Address' />
-            </label><br />
-            <label htmlFor='password'>Choose a Password: <br />
-                <input type='text' id='password' name='password' placeholder='Enter Password' />
-            </label><br />
-            <label htmlFor='terms'>Please Agree to the Terms and Conditions: <br />
-                <input type='checkbox' id='terms' name='terms' />
-            </label>
+            <form onSubmit={submitForm}>
+                <label htmlFor='name'>Name: <br />
+                    <input type='text' id='name' name='name' placeholder='Enter Name' onChange={handleChange} />
+                </label><br />
+                <label htmlFor='email'>Email: <br />
+                    <input type='text' id='email' name='email' placeholder='Enter Email Address' onChange={handleChange} />
+                </label><br />
+                <label htmlFor='password'>Choose a Password: <br />
+                    <input type='text' id='password' name='password' placeholder='Enter Password' onChange={handleChange} />
+                </label><br />
+                <label htmlFor='terms'>Please Agree to the Terms and Conditions: <br />
+                    <input type='checkbox' id='terms' name='terms' onChange={handleChange} />
+                </label><br />
+                <button type='submit' disabled={buttonDisabled} >Submit</button>
+            </form>
         </div>
     )
 }
